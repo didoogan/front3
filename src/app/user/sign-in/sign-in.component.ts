da6747f00bb9ae4c692e85930cbc6a112cadce3a
Dmitry Timoshenko <tzd0409@gmail.com>
2017-06-26 20:09:21 +0300

    LocalStorageService doesn't work

--- src/app/user/sign-in/sign-in.component.ts
+++ src/app/user/sign-in/sign-in.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {UserService} from "../user-service.service";
 import {Router} from "@angular/router";
-import {ENDPOINTS, MAIN_PAGE} from "../../helper/constants";
+import {MAIN_PAGE} from "../../helper/constants";
+import {LocalStorageService} from "ngx-webstorage";
 
 @Component({
   selector: 'app-sign-in',
@@ -9,28 +10,35 @@ import {ENDPOINTS, MAIN_PAGE} from "../../helper/constants";
   styleUrls: ['sign-in.component.scss']
 })
 export class SignInComponent implements OnInit {
-
-  user = {email: "", password: ""};
+    mainPage;
 
   constructor(
       private _userService: UserService,
-      private _router: Router
-  ) { }
-
-  signIn() {
-    this._userService.signIn(this.user.email, this.user.password)
-        .subscribe(
-      response => {
-        localStorage.setItem(
-            'currentUser',
-            JSON.stringify({ token: response.key, email: this.user.email })
-        );
-        window.location.reload();
-        this._router.navigate([MAIN_PAGE]);
-      },
-      error => console.log(error)
-    )
+      private _router: Router,
+      private _localStorage: LocalStorageService
+  ) {
+    this.mainPage = MAIN_PAGE;
   }
+
+
+    signIn() {
+        this._userService.signIn(this.user.email, this.user.password)
+        .subscribe(
+            response => {
+                // this._localStorage.store(
+                localStorage.setItem(
+                    'currentUser',
+                    JSON.stringify({ token: response.key, email: this.user.email })
+                );
+                window.location.reload();
+                // this._router.navigate([this.mainPage]);
+            },
+            error => console.log(error)
+        )
+    }
+
+  user = {email: "", password: ""};
+
 
   ngOnInit() {
   }
