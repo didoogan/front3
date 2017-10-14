d3c5b3bfd70ac040623bda7dfe03a835c139893a
Dmitry Timoshenko <tzd0409@gmail.com>
2017-10-15 00:50:14 +0300

    add profile model

--- src/app/user/profile/profile.component.ts
+++ src/app/user/profile/profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {AncestorModel} from "./model.profile";
+import {FormBuilder, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-profile',
@@ -7,30 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  url = "";
-  imgElem: any = null;
+    url = "";
+    imgElem: any = null;
+    private signInForm: FormGroup;
 
-  selectedTab: string = 'changeInfo';
 
-  constructor() { }
+    selectedTab: string = 'changeInfo';
+    ancestor: AncestorModel;
 
-  readUrl(event: any) {
-    if (event.target.files && event.target.files[0]) {
-      var reader = new FileReader();
-
-      reader.onload = (event: any) => {
-        this.url = event.target.result;
-      };
-
-      reader.readAsDataURL(event.target.files[0]);
+    constructor(private _fb: FormBuilder) {
     }
-  }
 
-  toggleTab() {
-    this.selectedTab = this.selectedTab == "changeInfo" ? "changePhoto" : "changeInfo"
-  }
+    readUrl(event: any) {
+        if (event.target.files && event.target.files[0]) {
+            let reader = new FileReader();
 
-  ngOnInit() {
-  }
+            reader.onload = (event: any) => {
+                this.url = event.target.result;
+            };
 
+            reader.readAsDataURL(event.target.files[0]);
+        }
+    }
+
+    toggleTab() {
+        this.selectedTab = this.selectedTab == "changeInfo" ? "changePhoto" : "changeInfo"
+    }
+
+    createAncestor() {
+        console.log(this.ancestor);
+    }
+
+    ngOnInit() {
+        this.ancestor = new AncestorModel();
+    }
 }
